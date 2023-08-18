334e46c2675a817eb1bbdf259cb02dce8b38de0a
zihfred <Lichkingblizzard@gmail.com>
2023-08-18 17:14:50 +0300

    added content, style fixes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 let closeButton = document.querySelector('.closeButton');
+let menuButtons = document.querySelectorAll('.menuLink');
 menuBtn.addEventListener('click', function(){
   menu.classList.toggle('active');
   menuBtn.classList.toggle('active');
   closeButton.classList.toggle('active');
 })
-
+menuButtons.forEach(button => {
+  button.addEventListener('click', function(){
+    menu.classList.toggle('active');
+    menuBtn.classList.toggle('active');
+    closeButton.classList.toggle('active');
+  })
+})
 closeButton.addEventListener('click', function(){
   menu.classList.toggle('active');
   menuBtn.classList.toggle('active');
